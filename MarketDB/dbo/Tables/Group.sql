2f07b3fab8d025c4b4afe5c0586de890fdc4b2d1
Kristina <bks1994@mail.ru>
2023-01-24 11:02:26 +0300

    add nvarchar(100) in Group

--- MarketDB/dbo/Tables/Group.sql
+++ MarketDB/dbo/Tables/Group.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Group] (
     [Id]        INT          IDENTITY (1, 1) NOT NULL,
-    [Name]      NVARCHAR (1) NOT NULL,
+    [Name]      NVARCHAR (100) NOT NULL,
     [Is_Delete] BIT          DEFAULT ('0') NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CHECK ([Name]<>''),
