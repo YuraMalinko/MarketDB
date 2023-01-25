3847b64d291d76560d5cf0edd603080a1d2ef9ec
Kristina <bks1994@mail.ru>
2023-01-25 18:34:10 +0300

    10-AddCommentToProductReview

---
+++ MarketDB/dbo/Stored Procedures/AddCommentToProductReview.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE AddCommentToProductReview
+@productId int,
+@clientId int,
+@comment nvarchar(1000)
+AS
+INSERT INTO [dbo].[ProductsReviews] ([Comment],[ClientId],[ProductId])
+VALUES (@comment, @clientId, @productId)
