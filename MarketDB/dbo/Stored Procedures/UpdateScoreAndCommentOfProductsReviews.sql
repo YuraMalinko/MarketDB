e10b573be8bda163761abe5581f01f4b562ed614
Kristina <bks1994@mail.ru>
2023-01-25 18:41:53 +0300

    10-UpdateScoreAndCommentOfProductsReviews

---
+++ MarketDB/dbo/Stored Procedures/UpdateScoreAndCommentOfProductsReviews.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE UpdateScoreAndCommentOfProductsReviews
+@productId int,
+@clientId int,
+@changeScore int,
+@changeComment nvarchar(1000)
+AS
+UPDATE [dbo].[ProductsReviews]
+SET [Comment]=@changeComment,
+[Score] = @changeScore
+WHERE
+[ProductId] = @productId AND [ClientId] = @clientId
