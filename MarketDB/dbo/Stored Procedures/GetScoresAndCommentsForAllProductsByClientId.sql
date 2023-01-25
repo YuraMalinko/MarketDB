cd1d7374047b46ae4b8f0f050159c8f95e55e09c
Kristina <bks1994@mail.ru>
2023-01-25 18:43:50 +0300

    10-GetScoresAndCommentsForAllProductsByClientId

---
+++ MarketDB/dbo/Stored Procedures/GetScoresAndCommentsForAllProductsByClientId.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE GetScoresAndCommentsForAllProductsByClientId
+@clientId int
+AS
+SELECT PR.ClientId, PR.ProductId, P.Name, PR.Score, PR.Comment
+FROM [dbo].[ProductsReviews] AS PR
+LEFT JOIN [dbo].[Products] AS P ON
+PR.ProductId=P.Id
+WHERE
+PR.ClientId = @clientId
