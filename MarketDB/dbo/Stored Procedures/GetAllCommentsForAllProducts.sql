6a2a27969fe839f021d05dde0b18fbb02daac96e
Kristina <bks1994@mail.ru>
2023-01-25 18:47:12 +0300

    10-GetAllCommentsForAllProducts

---
+++ MarketDB/dbo/Stored Procedures/GetAllCommentsForAllProducts.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE GetAllCommentsForAllProducts
+AS
+SELECT PR.ProductId, P.Name, PR.Comment, PR.ClientId
+FROM [dbo].[ProductsReviews] AS PR
+LEFT JOIN [dbo].[Products] AS P ON
+PR.ProductId=P.Id
