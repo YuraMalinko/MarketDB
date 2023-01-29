313f484695f1604219c67d141db63df0a5f69c89
malinkowork@gmail.com <malinkowork@gmail.com>
2023-01-29 19:06:16 +0300

    Add class for names requests a Manager table

---
+++ OfferAggregator.Dal/Options/StoredProcedures.cs
@@ -0,0 +1,11 @@
+﻿namespace OfferAggregator.Dal.Options
+{
+    public class StoredProcedures
+    {
+        public const string GetAllManagers = "GetAllManagers";
+        public const string AddManager = "AddManager";
+        public const string GetSingleManager = "GetSingleManager";
+        public const string DeleteManager = "DeleteManager";
+        public const string UpdateManager = "UpdateManager";
+    }
+}
