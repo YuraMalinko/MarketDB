e2086d7b0250042d95b5dd424c9729bb496929be
malinkowork@gmail.com <malinkowork@gmail.com>
2023-01-29 19:03:04 +0300

    Add new class for Connect to data base

---
+++ OfferAggregator.Dal/Options/ConnectOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferAggregator.Dal.Options
+{
+    public class ConnectOptions
+    {
+        public const string ConnectString = @"Data Source=80.78.240.16;
+                            Initial Catalog = MarketDataBase; 
+                            Persist Security Info=True;
+                            TrustServerCertificate=True;
+                            User ID = student;
+                            Password=qwe!23;";
+
+    }
+}
