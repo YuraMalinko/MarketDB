f30291c116ec9b57a53c92149d37384d0d8cb1f4
malinkowork@gmail.com <malinkowork@gmail.com>
2023-01-29 19:14:41 +0300

    Add new class Manager and 6 method for requests

---
+++ OfferAggregator.Dal/ManagerRepository.cs
@@ -0,0 +1,82 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using OfferAggregator.Dal.Models;
+using OfferAggregator.Dal.Options;
+using System.Data;
+
+namespace OfferAggregator.Dal
+{
+    public class ManagerRepository
+    {
+        public List<ManagerDto> GetAllManagers()
+        {
+            using (var sqlConnect = new SqlConnection(ConnectOptions.ConnectString))
+            {
+                sqlConnect.Open();
+                return sqlConnect.Query<ManagerDto>(
+                    StoredProcedures.GetAllManagers,
+                    commandType: CommandType.StoredProcedure).ToList();
+            }
+        }
+
+        public ManagerDto GetSingleManager(string login, string password)
+        {
+            using (var sqlConnect = new SqlConnection(ConnectOptions.ConnectString))
+            {
+                sqlConnect.Open();
+                return sqlConnect.QuerySingle<ManagerDto>(
+                    StoredProcedures.GetSingleManager,
+                    new { login, password },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public int AddManager(ManagerDto manager)
+        {
+            using (var sqlConnect = new SqlConnection(ConnectOptions.ConnectString))
+            {
+                sqlConnect.Open();
+                return sqlConnect.Execute(
+                    StoredProcedures.AddManager,
+                    new { manager.Login, manager.Password },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public int AddManagerDifferently(string login, string password)
+        {
+            using (var sqlConnect = new SqlConnection(ConnectOptions.ConnectString))
+            {
+                sqlConnect.Open();
+                return sqlConnect.Execute(
+                    StoredProcedures.AddManager,
+                    new { login, password },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public int UpdateManager(ManagerDto manager)
+        {
+            using (var sqlConnect = new SqlConnection(ConnectOptions.ConnectString))
+            {
+                sqlConnect.Open();
+                return sqlConnect.Execute(
+                    StoredProcedures.UpdateManager,
+                    new { manager.Id, manager.Login, manager.Password },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public int DeleteManager(int id)
+        {
+            using (var sqlConnect = new SqlConnection(ConnectOptions.ConnectString))
+            {
+                sqlConnect.Open();
+                return sqlConnect.Execute(
+                    StoredProcedures.DeleteManager,
+                    new { id },
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
