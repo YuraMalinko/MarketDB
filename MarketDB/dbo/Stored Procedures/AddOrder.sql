c34952046196fd693d040895a7f3c9b261406b5b
malinkowork@gmail.com <malinkowork@gmail.com>
2023-01-30 11:13:03 +0300

    add request create new order for table Orders

---
+++ MarketDB/dbo/Stored Procedures/AddOrder.sql
@@ -0,0 +1,10 @@
+﻿create procedure dbo.AddOrder
+	@DateCreate datetime2(0),
+	@ComplitionDate datetime2(0),
+	@ManagerId int,
+	@ClientId int
+as
+	insert [Orders] 
+	([DateCreate],[ComplitionDate],[IsDeleted],[ManagerId],[ClientId])
+	values
+	(@DateCreate,@ComplitionDate,0,@ManagerId,@ClientId)
