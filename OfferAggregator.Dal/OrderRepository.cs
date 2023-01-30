311f4fb228e2a4275c37b4d4b28018b8af35ebad
malinkowork@gmail.com <malinkowork@gmail.com>
2023-01-30 11:15:56 +0300

    add orders repository and method create new order

---
+++ OfferAggregator.Dal/OrderRepository.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using OfferAggregator.Dal.Models;
+using OfferAggregator.Dal.Options;
+using System.Data;
+
+namespace OfferAggregator.Dal
+{
+    public class OrderRepository
+    {
+        public int AddOrder(OrderDto order)
+        {
+            using (var SqlConnect = new SqlConnection(ConnectOptions.ConnectString))
+            {
+                SqlConnect.Open();
+                return SqlConnect.QuerySingle<int>(StoredProcedures.AddOrder,
+                    new
+                    {
+                        order.DateCreate,
+                        order.ComplitionDate,
+                        order.ManagerId,
+                        order.ClientId
+                    },
+                    commandType:CommandType.StoredProcedure);
+            }
+        }
+    }
+}
