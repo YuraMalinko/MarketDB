48a88ec3806dca8668067b066cf863670fd551b9
malinkowork@gmail.com <malinkowork@gmail.com>
2023-01-31 17:17:43 +0300

    add requests for get lists all orders and all orders by client Id

--- OfferAggregator.Console/Program.cs
+++ OfferAggregator.Console/Program.cs
@@ -6,14 +6,21 @@ using System.Security;
 OrderRepository OR = new OrderRepository();
 //OrderDto O = new OrderDto()
 //{
-//    DateCreate = DateTime.Now,
-//    ComplitionDate = new DateTime(2023, 2, 12, 13, 30, 0),
-//    ManagerId = 16,
+//    DateCreate = new DateTime(2026, 2, 13, 13, 30, 0),
+//    ComplitionDate = new DateTime(2027, 2, 13, 13, 30, 0),
+//    ManagerId = 5,
 //    ClientId = 2,
 //};
 
-var orders =OR.GetAllOrdersByIdManager(5);
+//O.Id=OR.AddOrder(O);
+//O.ManagerId = 6;
+//OR.UpdateOrder(O);
+
+//var orders = OR.GetAllOrders();
+
+//var blabla = OR.GetAllOrdersByClientId(2);
+
+
+OR.DeleteOrder(21);
 
 Console.ReadLine();
-
-//Console.WriteLine(O.Id=OR.AddOrder(O));
