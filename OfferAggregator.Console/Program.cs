93abd04c9242bc81b2ff2964470ca2263d3172bf
malinkowork@gmail.com <malinkowork@gmail.com>
2023-01-31 11:37:21 +0300

    whrite test methods to console

--- OfferAggregator.Console/Program.cs
+++ OfferAggregator.Console/Program.cs
@@ -4,12 +4,16 @@ using OfferAggregator.Dal.Models;
 using System.Security;
 
 OrderRepository OR = new OrderRepository();
-OrderDto O = new OrderDto()
-{
-    DateCreate = DateTime.Now,
-    ComplitionDate = new DateTime(2023, 2, 12, 13, 30, 0),
-    ManagerId = 16,
-    ClientId = 2,
-};
+//OrderDto O = new OrderDto()
+//{
+//    DateCreate = DateTime.Now,
+//    ComplitionDate = new DateTime(2023, 2, 12, 13, 30, 0),
+//    ManagerId = 16,
+//    ClientId = 2,
+//};
 
-Console.WriteLine(O.Id=OR.AddOrder(O));
+var orders =OR.GetAllOrdersByIdManager(5);
+
+Console.ReadLine();
+
+//Console.WriteLine(O.Id=OR.AddOrder(O));
