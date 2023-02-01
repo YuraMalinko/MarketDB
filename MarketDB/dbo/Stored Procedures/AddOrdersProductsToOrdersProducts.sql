a6ce2c61213df34752d341ac2c7e7b3f61046dd3
Kristina <bks1994@mail.ru>
2023-02-01 13:09:23 +0300

    9 - add AddOrdersProductsToOrdersProducts

---
+++ MarketDB/dbo/Stored Procedures/AddOrdersProductsToOrdersProducts.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[AddOrdersProductsToOrdersProducts]
+@orderId int,
+@productId int,
+@countProduct int
+AS
+INSERT INTO [dbo].[OrdersProducts] ([OrderId],[ProductId],[CountProduct])
+VALUES (@orderId, @productId, @countProduct)
