0b8d34b0a916f9afde1068a18e87aa7b7ed00f7f
Kristina <bks1994@mail.ru>
2023-02-01 22:12:56 +0300

    5- add dal AddProduct

---
+++ OfferAggregator.Dal/Repositories/TagsRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using OfferAggregator.Dal.Models;
+using System.Data;
+
+namespace OfferAggregator.Dal.Repositories
+{
+    public class TagsRepository
+    {
+        public int AddTag(string name)
+        {
+            using (var sqlCnctn = new SqlConnection(Options.ConnectionString))
+            {
+                sqlCnctn.Open();
+                return sqlCnctn.QuerySingle<int>(
+                    StoredProcedures.AddTag,
+                    new { name},
+                    commandType:CommandType.StoredProcedure);
+            }
+        }
+    }
+}
