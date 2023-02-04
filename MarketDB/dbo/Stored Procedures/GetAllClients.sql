fff30605de18e12ac6f5fc4ad78a2b9b760a38fd
malinkowork@gmail.com <malinkowork@gmail.com>
2023-02-04 17:49:22 +0300

    add request for get all clients

---
+++ MarketDB/dbo/Stored Procedures/GetAllClients.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[GetAllClients]
+AS
+SELECT
+C.[Id],
+C.[Name],
+C.[PhoneNumber],
+CFC.[Id],
+CFC.[Text]
+FROM [Clients] AS C
+LEFT JOIN [CommentsForClients] as CFC on C.Id=CFC.ClientId AND CFC.IsDeleted=0
+WHERE C.[IsDeleted]=0
