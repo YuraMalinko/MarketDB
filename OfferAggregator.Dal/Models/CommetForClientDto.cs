48d16493ae16d629a7cc234bf69bd1131491f5b9
malinkowork@gmail.com <malinkowork@gmail.com>
2023-02-04 17:51:24 +0300

    create new Dto models for Groups,CommentForClient and Tag tables

---
+++ OfferAggregator.Dal/Models/CommetForClientDto.cs
@@ -0,0 +1,11 @@
+﻿namespace OfferAggregator.Dal.Models
+{
+    public class CommetForClientDto
+    {
+        public int? Id { get; set; }
+
+        public string? Text { get; set; }
+
+        public int? ClientId { get; set; }
+    }
+}
---
+++ OfferAggregator.Dal/Models/TagDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfferAggregator.Dal.Models
+{
+    public class TagDto
+    {
+        public int? Id { get; set; }
+
+        public string? Name { get; set; }
+
+    }
+}
