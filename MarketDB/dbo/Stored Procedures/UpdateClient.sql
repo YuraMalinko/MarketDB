f544d1369a7c246f76fcb7bb1d2add14f55ee8b4
malinkowork@gmail.com <malinkowork@gmail.com>
2023-02-04 17:48:19 +0300

    add request for update client

---
+++ MarketDB/dbo/Stored Procedures/UpdateClient.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[UpdateClient]
+@Id int,
+@Name nvarchar (100),
+@PhoneNumber nvarchar(20)
+AS
+UPDATE [Clients] 
+SET [Name] = @Name,
+[PhoneNumber] = @PhoneNumber
+WHERE [Id]=@Id
