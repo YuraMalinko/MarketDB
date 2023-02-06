9fa8adb6f7f98116d9acca17d6e292311bdccd3b
Kristina <bks1994@mail.ru>
2023-02-06 18:33:01 +0300

    5 - fix codeStyle

--- OfferAggregator.Dal/StoredProcedures.cs
+++ OfferAggregator.Dal/StoredProcedures.cs
@@ -4,71 +4,105 @@
     {
         public const string AddProduct = "AddProduct";
 
+
         public const string GetAllProducts = "GetAllProducts";
+
 
         public const string GetAllProductsByGroupId = "GetAllProductsByGroupId";
 
+
         public const string UpdateProduct = "UpdateProduct";
+
 
         public const string DeleteProduct = "DeleteProduct";
 
+
         public const string AddAmountToStocks = "AddAmountToStocks";
+
 
         public const string AddScoreAndCommentToProductReview = "AddScoreAndCommentToProductReview";
 
+
         public const string GetAllScoresAndCommentsForProducts = "GetAllScoresAndCommentsForProducts";
+
 
         public const string GetAmountByProductId = "GetAmountByProductId";
 
+
         public const string GetAmountsOfAllProducts = "GetAmountsOfAllProducts";
+
 
         public const string GetAllScoresAndCommentsForProductsByClientId = "GetAllScoresAndCommentsForProductsByClientId";
 
+
         public const string GetAllScoresAndCommentsForProductByProductId = "GetAllScoresAndCommentsForProductByProductId";
+
 
         public const string GetAllScoresAndCommentsForProductByProductIDAndClientId = "GetAllScoresAndCommentsForProductByProductIDAndClientId";
 
+
         public const string UpdateAmountOfStocks = "UpdateAmountOfStocks";
+
 
         public const string UpdateScoreAndCommentOfProductsReviews = "UpdateScoreAndCommentOfProductsReviews";
 
+
         public const string GetAllManagers = "GetAllManagers";
+
 
         public const string AddManager = "AddManager";
 
+
         public const string GetSingleManager = "GetSingleManager";
+
 
         public const string DeleteManager = "DeleteManager";
 
+
         public const string UpdateManager = "UpdateManager";
+
 
         public const string AddOrder = "AddOrder";
 
+
         public const string DeleteOrder = "DeleteOrder";
+
 
         public const string UpdateOrder = "UpdateOrder";
 
+
         public const string GetAllOrdersByIdManager = "GetAllOrdersByIdManager";
+
 
         public const string GetAllOrdersByClientId = "GetAllOrdersByClientId";
 
+
         public const string GetAllOrders = "GetAllOrders";
+
 
         public const string AddTag = "AddTag";
 
+
         public const string AddTagProduct = "AddTagProduct";
+
 
         public const string GetAllTags = "GetAllTags";
 
+
         public const string GetAllTagsByProductId = "GetAllTagsByProductId";
+
 
         public const string UpdateTagName = "UpdateTagName";
 
+
         public const string DeleteTag = "DeleteTag";
+
 
         public const string DeleteProductsReviews = "DeleteProductsReviews";
 
+
         public const string DeleteStock = "DeleteStock";
+
 
         public const string DeleteTagProduct = "DeleteTagProduct";
     }
