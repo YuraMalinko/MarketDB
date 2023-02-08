ba9d4509591dff0a2f7ca74f5fea7b67a8721dd6
Kristina <bks1994@mail.ru>
2023-02-08 22:23:38 +0300

    add  UNIQUE NONCLUSTERED ([PhoneNumber])

--- MarketDB/dbo/Tables/Clients.sql
+++ MarketDB/dbo/Tables/Clients.sql
@@ -4,4 +4,7 @@
     [PhoneNumber] NVARCHAR (20)  NOT NULL,
     [IsDeleted]  BIT            DEFAULT ('0') NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
+    UNIQUE NONCLUSTERED ([PhoneNumber])
+    
+
 );
