351c7dc55bd2143f2181a87d9b4f80c5dbfe847a
Ketrin <kyoto2020@yandex.ru>
2023-02-09 21:36:35 +0300

    create client class and meth for get all client

--- OfferAggregator.Console/Program.cs
+++ OfferAggregator.Console/Program.cs
@@ -3,6 +3,15 @@ using OfferAggregator.Dal.Repositories;
 using OfferAggregator.Bll;
 using OfferAggregator.Bll.Models;
 
+ClientsDto clin = new ClientsDto()
+{
+    
+};
+
+
+ClientService clt = new ClientService();
+
+var ccc = clt.GetAllClientsWithoutComment();
 
 Console.WriteLine();
 
