ae1a8e30dff1ab07251f9c006f6206d6a31ba65d
Ketrin <kyoto2020@yandex.ru>
2023-02-09 21:47:05 +0300

    testforPULLreqests

--- OfferAggregator.Console/Program.cs
+++ OfferAggregator.Console/Program.cs
@@ -3,6 +3,16 @@ using OfferAggregator.Dal.Repositories;
 using OfferAggregator.Bll;
 using OfferAggregator.Bll.Models;
 
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+
+ClientService clt = new ClientService();
+
+var ccc = clt.GetAllClientsWithoutComment();
 
 Console.WriteLine();
 
