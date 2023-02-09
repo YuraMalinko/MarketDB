26259237dbc152251b169bff4f68c59e82dea00d
malinkowork@gmail.com <malinkowork@gmail.com>
2023-02-09 09:53:03 +0300

    add class for Mapper

--- OfferAggregator.Bll/Mapper.cs
+++ OfferAggregator.Bll/Mapper.cs
@@ -1,13 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AutoMapper;
+using OfferAggregator.Bll.Models;
+using OfferAggregator.Dal.Models;
 
 namespace OfferAggregator.Bll
 {
     public class Mapper
     {
-        public Mapper() { }
+        private MapperConfiguration _config;
+
+        private static Mapper _instance;
+
+        private Mapper()
+        {
+            _config = new MapperConfiguration(
+                cfg =>
+                {
+                    cfg.CreateMap<ManagerAuthInput, ManagerDto>();
+
+                    cfg.CreateMap<ManagerInput, ManagerDto>();
+
+                    cfg.CreateMap<ManagerDto, ManagerOutput>();
+
+                });
+        }
+
+        public static Mapper GetInstance()
+        {
+            if (_instance is null)
+            {
+                _instance = new Mapper();
+            }
+
+            return _instance;
+        }
+
+        public ManagerDto MapManagerAuthInputToManagerDto(ManagerAuthInput manager)
+        {
+            return _config.CreateMapper().Map<ManagerDto>(manager);
+        }
+
+        public ManagerAuthOutput MapManagerDtoToManagerAuthOutput(ManagerDto manager)
+        {
+            return _config.CreateMapper().Map<ManagerAuthOutput>(manager);
+        }
+
+        public List<ManagerOutput> MapManagerDtoToManagerOutput(List<ManagerDto> manager)
+        {
+            return _config.CreateMapper().Map<List<ManagerOutput>>(manager);
+        }
+
+        public ManagerDto MapManagerInputToManagerDto(ManagerInput manager)
+        {
+            return _config.CreateMapper().Map<ManagerDto>(manager);
+        }
+
+
     }
 }
