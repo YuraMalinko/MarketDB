c074f0c28d0cdae8f86f29527df5221f18847b72
Kristina <bks1994@mail.ru>
2023-02-16 14:33:49 +0300

    fix bugs in ProductsRepository

--- OfferAggregator.Bll/OrderService.cs
+++ OfferAggregator.Bll/OrderService.cs
@@ -42,6 +42,72 @@ namespace OfferAggregator.Bll
             _commentForClientRepository = commentForClientRepository;
         }
 
+        //public int CreateNewOrder(CreatingOrderModel creatingOrderModel)
+        //{
+        //    try
+        //    {
+        //        ManagerDto getManager = _managerRepository.GetManagerById(creatingOrderModel.Order.ManagerId);
+        //        ClientsDto getClient = _clientRepository.GetClientById(creatingOrderModel.Order.ClientId);
+        //        if (getManager != null && getClient != null && creatingOrderModel.Order.DateCreate < creatingOrderModel.Order.ComplitionDate)
+        //        {
+        //            CreatingOrderDto creatingOrderDto = _instanceMapper.MapCreatingOrderModelToCreatingOrderDto(creatingOrderModel);
+        //            int addOrder = _orderRepository.AddOrder(creatingOrderDto.Order);
+        //            creatingOrderModel.Order.Id = addOrder;
+
+        //            if (creatingOrderDto.CommentsForOrder != null)
+        //            {
+        //                foreach (var crnt in creatingOrderDto.CommentsForOrder)
+        //                {
+        //                    crnt.OrderId = addOrder;
+        //                    int addCommentForOrder = _commentForOrderRepository.AddCommentOrder(crnt);
+        //                }
+        //            }
+
+        //            if (creatingOrderDto.CommentsForClient != null)
+        //            {
+        //                foreach (var crnt in creatingOrderDto.CommentsForClient)
+        //                {
+        //                    int addCommentForClient = _commentForClientRepository.AddComment(crnt);
+        //                }
+        //            }
+
+        //            List<ProductCountModel> getProducts = creatingOrderModel.Products;
+
+        //            if (getProducts != null)
+        //            {
+        //                foreach (var crnt in getProducts)
+        //                {
+        //                    var getProductById = _productsRepository.GetProductById(crnt.Id);
+        //                    if (getProductById != null)
+        //                    {
+        //                        OrdersProductsDto ordersProductsDto = new OrdersProductsDto
+        //                        {
+        //                            OrderId = creatingOrderModel.Order.Id,
+        //                            ProductId = crnt.Id,
+        //                            CountProduct = crnt.Count
+        //                        };
+        //                        bool addProductToOrder = _ordersProductsRepository.AddProductToOrders(ordersProductsDto);
+        //                    }
+        //                    else
+        //                    {
+        //                        return -1;
+        //                    };
+        //                }
+        //            }
+
+        //            return addOrder;
+        //        }
+        //        else
+        //        {
+        //            return -1;
+        //        };
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return -1;
+        //    }
+        //}
+
         public int CreateNewOrder(CreatingOrderModel creatingOrderModel)
         {
             try
@@ -53,7 +119,7 @@ namespace OfferAggregator.Bll
                     CreatingOrderDto creatingOrderDto = _instanceMapper.MapCreatingOrderModelToCreatingOrderDto(creatingOrderModel);
                     int addOrder = _orderRepository.AddOrder(creatingOrderDto.Order);
                     creatingOrderModel.Order.Id = addOrder;
-                    
+
                     if (creatingOrderDto.CommentsForOrder != null)
                     {
                         foreach (var crnt in creatingOrderDto.CommentsForOrder)
