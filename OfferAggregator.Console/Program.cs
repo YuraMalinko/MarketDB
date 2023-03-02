3bf84413c0e60b676e786afada56719bec170484
malinkowork@gmail.com <malinkowork@gmail.com>
2023-03-02 12:13:30 +0300

    fix bug

--- OfferAggregator.Console/Program.cs
+++ OfferAggregator.Console/Program.cs
@@ -6,23 +6,6 @@ using OfferAggregator.Bll;
 using OfferAggregator.Bll.Models;
 using System.Net.Http.Headers;
 
-ClientRepository _clientRepository = new ClientRepository();
-ClientsWishesRepository _clientsWishesRepository = new ClientsWishesRepository();
-CommentForClientRepository _commentForClientRepository = new CommentForClientRepository();
-OrderRepository _orderRepository = new OrderRepository();
-
-ClientService clt = new ClientService(_clientRepository);
-
-ClientOutputModel newclt = new ClientOutputModel() { Id = 352, Name = "Kevin", PhoneNumber = "38286387" };
-
-//var ccc = clt.AddClient(newclt);
-
-var sss = clt.GetAllClientsWithoutComment();
-
-var del = clt.DeleteClient(13);
-
-var s = clt.GetAllClientsWithoutComment();
-
 Console.WriteLine( );
 
 AggregatorRepository AR = new AggregatorRepository();
