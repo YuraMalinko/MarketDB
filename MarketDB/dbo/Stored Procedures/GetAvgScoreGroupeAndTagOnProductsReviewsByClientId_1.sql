478129161bff285c3a3a2a70a9f3ae48d80a027c
malinkowork@gmail.com <malinkowork@gmail.com>
2023-03-06 02:22:00 +0300

    Add stored procedures for get statistic for combo taggroup

--- MarketDB/dbo/Stored Procedures/GetAvgScoreGroupeAndTagOnProductsReviewsByClientId_1.sql
+++ MarketDB/dbo/Stored Procedures/GetAvgScoreGroupeAndTagOnProductsReviewsByClientId_1.sql
@@ -1,12 +1,13 @@
 ﻿CREATE PROCEDURE [dbo].[GetAvgScoreGroupeAndTagOnProductsReviewsByClientId]
 @ClientId int
 AS
-SELECT ROUND(AVG(CAST(ISNULL(PR.Score,3) AS FLOAT)),1) AS AvgScore ,G.[Id],G.[Name],T.[Id],T.[Name]
+SELECT ROUND(AVG(CAST(ISNULL(PR.Score,3) AS FLOAT)),1) AS AvgScore ,G.[Id] as GroupId,T.[Id] AS TagId
 FROM dbo.[ProductsReviews] AS PR
 INNER JOIN Products AS P ON PR.[ProductId]=P.[Id]
 INNER JOIN dbo.[Groups] AS G ON P.[GroupId]=G.[Id] and G.IsDeleted=0
 LEFT JOIN dbo.[TagsProducts] AS TP ON TP.[ProductId]=P.[Id]
 LEFT JOIN dbo.[Tags] AS T ON TP.[TagId]=T.[Id]
-WHERE PR.[ClientId]=@ClientId and (T.[IsDeleted] = 0 or T.[IsDeleted] is null)
-GROUP BY G.[Id],G.[Name],T.[Id],T.[Name]
+WHERE PR.[ClientId]=@ClientId 
+AND (T.[IsDeleted] = 0 or T.[Id] is null)
+GROUP BY G.[Id],T.[Id]
 ORDER BY AvgScore DESC
---
+++ MarketDB/dbo/Stored Procedures/GetComboTagGroupOfLikeOrDislikeByClientId_.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[GetComboTagGroupOfLikeOrDislikeByClientId]
+@ClientId int
+AS
+SELECT CW.[IsLiked],G.[Id] as GroupId,T.[Id] AS TagId FROM dbo.[ClientsWishes] AS CW
+LEFT JOIN dbo.[Groups] AS G ON CW.[GroupId]=G.[Id]
+LEFT JOIN dbo.[Tags] AS T ON CW.[TagId]=T.[Id]
+WHERE CW.[ClientId]=@ClientId
+AND (G.[IsDeleted] = 0 or G.[Id] is null)
+AND (T.[IsDeleted] = 0 or T.[Id] is null)
