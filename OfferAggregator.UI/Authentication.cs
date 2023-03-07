db869d4a0c9b2c4716245a070c87e8a0467f2225
Ketrin <leanworld2020@gmail.com>
2023-03-07 15:17:34 +0300

    maybe

---
+++ OfferAggregator.UI/Authentication.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace OfferAggregator.UI
+{
+    public class Authentication : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity();
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+
+        public void AuthenticateUser(string name, int id)
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+            new Claim(ClaimTypes.Name, name),
+            new Claim("ID", id.ToString()),
+            }, "Custom Authentication");
+
+            var user = new ClaimsPrincipal(identity);
+
+            NotifyAuthenticationStateChanged(
+                Task.FromResult(new AuthenticationState(user)));
+        }
+    }
+}
--- OfferAggregator.UI/Program.cs
+++ OfferAggregator.UI/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Authorization;
+using OfferAggregator.UI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<AuthenticationStateProvider, Authentication>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
